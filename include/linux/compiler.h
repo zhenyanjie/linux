9caf452885c863cbd7dda27d3aa7a156df01f5b0
Junjie Mao <eternal.n08@gmail.com>
2015-01-26 15:57:44 +0800

    Convert likely/unlikely to a easier-to-be-analyzed version
    Signed-off-by: Junjie Mao <eternal.n08@gmail.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -156,8 +156,10 @@ void ftrace_likely_update(struct ftrace_branch_data *f, int val, int expect);
 #endif /* CONFIG_PROFILE_ALL_BRANCHES */
 
 #else
-# define likely(x)	__builtin_expect(!!(x), 1)
-# define unlikely(x)	__builtin_expect(!!(x), 0)
+/* # define likely(x)	__builtin_expect(!!(x), 1) */
+/* # define unlikely(x)	__builtin_expect(!!(x), 0) */
+# define likely(x)	(x)
+# define unlikely(x)	(x)
 #endif
 
 /* Optimization barrier */
