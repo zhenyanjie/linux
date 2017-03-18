92e65a488ac0ed82dadfab4b1d617f8235d85402
Mauro Rossi <issor.oruam@gmail.com>
2017-03-18 19:08:15 +0100

    Revert "Enable user xattr for tmpfs"
    This reverts commit e7819ea612ceab655b8fb243a8f4dcd99f6093c8.

--- mm/shmem.c
+++ mm/shmem.c
@@ -3207,12 +3207,6 @@ static const struct xattr_handler shmem_trusted_xattr_handler = {
 	.set = shmem_xattr_handler_set,
 };
 
-static const struct xattr_handler shmem_user_xattr_handler = {
-	.prefix = XATTR_USER_PREFIX,
-	.get = shmem_xattr_handler_get,
-	.set = shmem_xattr_handler_set,
-};
-
 static const struct xattr_handler *shmem_xattr_handlers[] = {
 #ifdef CONFIG_TMPFS_POSIX_ACL
 	&posix_acl_access_xattr_handler,
@@ -3221,7 +3215,6 @@ static const struct xattr_handler *shmem_xattr_handlers[] = {
 	&shmem_user_xattr_handler,
 	&shmem_security_xattr_handler,
 	&shmem_trusted_xattr_handler,
-       &shmem_user_xattr_handler,
 	NULL
 };
 
